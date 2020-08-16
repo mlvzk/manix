31cfd84496edd647366b1ef50d4acbe37576ae5c
mlvzk <mlvzk@protonmail.com>
2020-08-16 15:55:12 +0200

    fail safe when can't generate nixos options

--- src/bin/manix.rs
+++ src/bin/manix.rs
@@ -25,40 +25,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     if cache_invalid {
+        if let Ok(options_db) = options_docsource::get_hm_json_doc_path()
+            .ok()
+            .and_then(|path| OptionsDatabase::try_from_file(path))
+            .ok_or(CustomError(
+                "Failed to load Home Manager options".to_string(),
+            ))
         {
-            let options_db = options_docsource::get_hm_json_doc_path()
-                .ok()
-                .and_then(|path| OptionsDatabase::try_from_file(path))
-                .ok_or(CustomError("".to_string()))?;
             let out = bincode::serialize(&options_db)
                 .map_err(|_| CustomError("Failed to serialize cache".into()))?;
             std::fs::write(&options_hm_cache_path, out)
                 .map_err(|_| CustomError("Failed to write cache to file".into()))?;
             aggregate_source.add_source(Box::new(options_db));
         }
+
+        match options_docsource::get_nixos_json_doc_path()
+            .ok()
+            .and_then(|path| OptionsDatabase::try_from_file(path))
+            .ok_or(CustomError("Failed to load NixOS options".to_string()))
         {
-            let options_db = options_docsource::get_nixos_json_doc_path()
-                .ok()
-                .and_then(|path| OptionsDatabase::try_from_file(path))
-                .ok_or(CustomError("Failed to load NixOS options".to_string()))?;
-            let out = bincode::serialize(&options_db)
-                .map_err(|_| CustomError("Failed to serialize cache".into()))?;
-            std::fs::write(&options_nixos_cache_path, out)
-                .map_err(|_| CustomError("Failed to write cache to file".into()))?;
-            aggregate_source.add_source(Box::new(options_db));
+            Ok(options_db) => {
+                let out = bincode::serialize(&options_db)
+                    .map_err(|_| CustomError("Failed to serialize cache".into()))?;
+                std::fs::write(&options_nixos_cache_path, out)
+                    .map_err(|_| CustomError("Failed to write cache to file".into()))?;
+                aggregate_source.add_source(Box::new(options_db));
+            }
+            Err(e) => eprintln!("Failed to load NixOS options: {:#?}", e),
         }
     } else {
+        if let Ok(cache_bin) = std::fs::read(&options_hm_cache_path)
+            .map_err(|_| CustomError("Failed to read the cache file HM".into()))
         {
-            let cache_bin = std::fs::read(&options_hm_cache_path)
-                .map_err(|_| CustomError("Failed to read the cache file HM".into()))?;
             let options_db: OptionsDatabase = bincode::deserialize(&cache_bin)
                 .map_err(|_| CustomError("Failed to deserialize cache".into()))?;
             aggregate_source.add_source(Box::new(options_db));
         }
 
+        if let Ok(cache_bin) = std::fs::read(&options_nixos_cache_path)
+            .map_err(|_| CustomError("Failed to read the cache file NIXOS".into()))
         {
-            let cache_bin = std::fs::read(&options_nixos_cache_path)
-                .map_err(|_| CustomError("Failed to read the cache file NIXOS".into()))?;
             let options_db: OptionsDatabase = bincode::deserialize(&cache_bin)
                 .map_err(|_| CustomError("Failed to deserialize cache".into()))?;
             aggregate_source.add_source(Box::new(options_db));

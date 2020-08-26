ea3839f4f77e928472fadf6b05fd64e6fa122e38
mlvzk <mlvzk@protonmail.com>
2020-08-26 16:47:51 +0200

    add Debug to Nixpkgs Tree DocSource

--- src/nixpkgs_tree_docsource.rs
+++ src/nixpkgs_tree_docsource.rs
@@ -2,7 +2,7 @@ use crate::{Cache, DocEntry, DocSource, Errors};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, process::Command};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct NixpkgsTreeDatabase {
     keys: Vec<String>,
 }

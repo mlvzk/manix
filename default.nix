bfcb426d19d63c6c876068ba1467297660d6cb76
mlvzk <mlvzk@protonmail.com>
2020-09-13 15:22:30 +0200

    use pkgs.rustPlatform instead of naersk, add CI

--- default.nix
+++ default.nix
@@ -1,5 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-    sources = import ./nix/sources.nix;
-    naersk = pkgs.callPackage sources.naersk {};
-in naersk.buildPackage ./.
+{ pkgs ? import <nixpkgs> {}
+}: pkgs.rustPlatform.buildRustPackage rec {
+  pname = "manix";
+  version = "0.6.0";
+
+  src = ./.;
+  cargoSha256 = "078v4wgblc1cr5d1hk42i7qac1865bnm26cxxlvc6aya40ryxbv3";
+}

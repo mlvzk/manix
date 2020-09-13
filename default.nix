bcd4df7129df092d0516a6583f70e1eb76fe3162
mlvzk <mlvzk@protonmail.com>
2020-09-13 15:42:45 +0200

    pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
-{ pkgs ? import <nixpkgs> {}
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs {}
 }: pkgs.rustPlatform.buildRustPackage rec {
   pname = "manix";
   version = "0.6.0";
 
   src = ./.;
-  cargoSha256 = "078v4wgblc1cr5d1hk42i7qac1865bnm26cxxlvc6aya40ryxbv3";
+  cargoSha256 = "0wpc65cl98lh2zdgrwxg07hhvfkmhwkb0xzyg6rd1v6xnh13g01j";
 }

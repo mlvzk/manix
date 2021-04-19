abfb06d3f9057d33eb3c72412d05370d30868637
mlvzk <mlvzk@protonmail.com>
2021-04-20 01:44:21 +0200

    fix NixOS Options doc source build

--- src/options_docsource.rs
+++ src/options_docsource.rs
@@ -121,6 +121,7 @@ pub fn get_nixos_json_doc_path() -> Result<PathBuf, std::io::Error> {
     let base_path_output = Command::new("nix-build")
         .env("NIXPKGS_ALLOW_UNFREE", "1")
         .env("NIXPKGS_ALLOW_BROKEN", "1")
+        .env("NIXPKGS_ALLOW_INSECURE", "1")
         .arg("--no-out-link")
         .arg("-E")
         .arg(r#"with import <nixpkgs> {}; let eval = import (pkgs.path + "/nixos/lib/eval-config.nix") { modules = []; }; opts = (nixosOptionsDoc { options = eval.options; }).optionsJSON; in runCommandLocal "options.json" { inherit opts; } "cp $opts/share/doc/nixos/options.json $out""#)
